c9b52b67da3e719e1d4978405e5ce31bee5072e0
SoloOleh <155486534+SoloOleh@users.noreply.github.com>
2024-05-03 19:05:25 +0300

    03.05.2024

--- Task_1.py
+++ Task_1.py
@@ -47,16 +47,4 @@ for size in test_data_sizes:
 
 results
 
-print(results)  
-
-"""
-Test data size: 100, Insertion sort time: 0.00010254199150949717, Merge sort time: 8.512497879564762e-05, Phyton timsort time: 8.041970431804657e-06, 
-Test data size: 1000, Insertion sort time: 0.011608750035520643, Merge sort time: 0.0010459170443937182, Phyton timsort time: 7.887498941272497e-05, 
-Test data size: 10000, Insertion sort time: 1.23389741696883, Merge sort time: 0.012771415989845991, Phyton timsort time: 0.0009039579890668392.
-"""
-
-""" 
-Timsort ефективно використовує переваги обох сортувань: вставками на малих сегментах даних та злиттям для більших. 
-Це робить його швидким і ефективним навіть на великих масивах. 
-Тому більшість людей використовують вбудовані алгоритми сортування в Python, а не пишуть свої. 
-"""
+print(results)
